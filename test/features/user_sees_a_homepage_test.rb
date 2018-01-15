d7f3876efe494f0a10cfba759f6376d466fceefc
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-15 15:31:23 -0700

    Adds error page for unknown locations

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_can_not_visit_nonexistent_page
+    visit '/blog'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
 end
